62abb54127e56604ed7dd31410dbcbc72d6401da
Antoine Champion <achampion@salesforce.com>
2026-05-11 17:16:05 +0200

    feat(ui): add featured game view

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,26 +1,13 @@
 <script setup lang="ts">
-import ArcadeButton from './components/ArcadeButton.vue'
+import FeaturedGame from './components/FeaturedGame.vue'
 </script>
 
 <template>
-  <div class="flex items-center justify-center min-h-screen gap-8">
-    <ArcadeButton>
-      <template #icon>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-          <path d="M12 2L9.5 9.5H2l6 4.5L5.5 22 12 17l6.5 5-2.5-8 6-4.5h-7.5z" />
-        </svg>
-      </template>
-    </ArcadeButton>
-
-    <ArcadeButton label="Play" />
-
-    <ArcadeButton label="Settings">
-      <template #icon>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-          <path d="M12 15.5A3.5 3.5 0 1 0 12 8.5a3.5 3.5 0 0 0 0 7z" />
-          <path d="M19.43 12.98c.04-.32.07-.64.07-.98s-.03-.66-.07-.98l2.11-1.65a.5.5 0 0 0 .12-.64l-2-3.46a.5.5 0 0 0-.61-.22l-2.49 1a7.04 7.04 0 0 0-1.69-.98l-.38-2.65A.49.49 0 0 0 14 2h-4a.49.49 0 0 0-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1a.5.5 0 0 0-.61.22l-2 3.46a.49.49 0 0 0 .12.64l2.11 1.65c-.04.32-.07.65-.07.98s.03.66.07.98l-2.11 1.65a.5.5 0 0 0-.12.64l2 3.46c.12.22.39.3.61.22l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.05.24.26.42.49.42h4c.24 0 .44-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.22.08.49 0 .61-.22l2-3.46a.5.5 0 0 0-.12-.64l-2.11-1.65z" />
-        </svg>
-      </template>
-    </ArcadeButton>
-  </div>
+  <FeaturedGame
+    title="Street Fighter III: 3rd Strike"
+    platform="Fightcade"
+    :release-year="1999"
+    developer="Capcom"
+    banner-url="https://d1lss44hh2trtw.cloudfront.net/resize?type=webp&url=https%3A%2F%2Fshacknews-www.s3.amazonaws.com%2Fassets%2Farticle%2F2024%2F07%2F29%2F3rd-strike-header_feature.jpg&width=1032&sign=XEMEx9JTW7qcjq1YKxMlyIOL40hjUM0eBKwjqW6KlMU"
+  />
 </template>
---
+++ front-end/src/components/FeaturedGame.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import ArcadeButton from './ArcadeButton.vue'
+
+defineProps<{
+  title: string
+  platform: string
+  releaseYear: number
+  developer: string
+  bannerUrl: string
+}>()
+</script>
+
+<template>
+  <section class="relative h-[60vh] overflow-hidden">
+    <div class="absolute inset-0">
+      <img :src="bannerUrl" :alt="title" class="banner-img h-full object-cover w-[80%] ml-[30%]" />
+    </div>
+
+    <div class="relative flex flex-col items-start justify-end h-full p-24 gap-3">
+      <span class="text-sm font-medium uppercase tracking-widest opacity-70 ml-0.5">{{ platform }}</span>
+      <h1 class="text-5xl font-bold leading-tight">{{ title }}</h1>
+      <span class="text-sm opacity-60 ml-0.5">{{ releaseYear }} - {{ developer }}</span>
+      <ArcadeButton class="mt-9" label="Play">
+        <template #icon>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-5">
+            <path d="M8 5v14l11-7z" />
+          </svg>
+        </template>
+      </ArcadeButton>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.banner-img {
+  animation: pan 30s linear infinite alternate;
+  mask-image:
+    linear-gradient(to right, transparent 0%, black 40%),
+    linear-gradient(to top, transparent 0%, black 40%);
+  mask-composite: intersect;
+  -webkit-mask-image:
+    linear-gradient(to right, transparent 0%, black 40%),
+    linear-gradient(to top, transparent 0%, black 40%);
+  -webkit-mask-composite: source-in;
+}
+
+@keyframes pan {
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(-16.67%);
+  }
+}
+</style>
