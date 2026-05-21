cc14a5eca6d7ab1a29a97000681ab8cc0fcdd8e7
Antoine Champion <achampion@salesforce.com>
2026-05-21 15:12:37 +0200

    fix(tauri): true fullscreen on Wayland

--- tauri/src/main.rs
+++ tauri/src/main.rs
@@ -2,6 +2,13 @@
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            use tauri::Manager;
+            if let Some(window) = app.get_webview_window("main") {
+                let _ = window.set_fullscreen(true);
+            }
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
